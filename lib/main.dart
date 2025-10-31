217f9852940be68a3f6a0564bcd643bdc651ee7d
I Ketut Gunawan <iketut.gunawan@vida.id>
2025-10-31 20:18:51 +0700

    update MaterialApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,14 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(MyMaterialApp());
+}
+
+class MyMaterialApp extends StatelessWidget {
+  const MyMaterialApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
